1ab89ef62621541ff3706f5014db8657436e88c2
ofelyagrigoryan-dot <ofelya.grigoryan@tumo.org>
2026-07-11 13:47:22 +0400

    first commit

---
+++ audio.js
@@ -0,0 +1,47 @@
+var date = {
+    title: [
+        "So Far So Fake",
+        "Call Out my name",
+        "Do I wanna know",
+        "Freaked out",
+        "Gladiator",
+        "Mala ft Anuel AA",
+        "São Paulo",
+        "Secret",
+        "Swim",
+        "The Walls",
+        "Timeless"
+
+    ],
+    song: [
+        "music/So Far So Fake.mp3",
+        "music/Call Out my name.mp3",
+        "music/Do I wanna know.mp3",
+        "music/Freaked out.mp3",
+        "music/Gladiator.mp3",
+        "music/Mala ft Anuel AA.mp3",
+        "music/São Paulo.mp3",
+        "music/Secret.mp3",
+        "music/Swim.mp3",
+        "music/The Walls.mp3",
+        "music/Timeless.mp3"
+    ],
+    
+    poster: [
+        "https://i.pinimg.com/736x/3c/8d/a4/3c8da421c0a1aa85804bf6aa23f02a2b.jpg",
+        "https://i.pinimg.com/1200x/c6/b2/33/c6b2339d79aa7bf4ad318235d19b618f.jpg",
+        "https://i.pinimg.com/1200x/fc/2e/22/fc2e22b867d7ccf7158aeb7b56ec9a40.jpg",
+        "https://i.pinimg.com/1200x/71/65/b5/7165b53977fb6cbb554ec8c64459ccc1.jpg",
+        "https://i.pinimg.com/1200x/bb/48/fc/bb48fc0e9cafa092726bd66293a3dcba.jpg",
+        "https://i.pinimg.com/1200x/36/c5/4e/36c54e42ce212d7c3b756dfbacd18461.jpg",
+        "https://i.pinimg.com/736x/8e/ea/84/8eea8490e04f679f73a90bb54bb7a7a3.jpg",
+        "https://i.pinimg.com/1200x/5f/51/1c/5f511ce336e77c530fbf45633e72cf76.jpg",
+        "https://i.pinimg.com/736x/58/05/dc/5805dc720619a9b82733a02e73009670.jpg",
+        "https://i.pinimg.com/736x/d6/88/a7/d688a74eeb21e7ac30feb2589b92c184.jpg",
+        "https://i.pinimg.com/736x/b1/71/37/b171374154470dd6b024bf3b29bc9479.jpg"
+    
+    ]
+
+    
+}
+
