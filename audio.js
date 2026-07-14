24d576772b5442f51cc817dc3478dd31d37884b4
ofelyagrigoryan-dot <ofelya.grigoryan@tumo.org>
2026-07-14 14:22:16 +0400

    function

--- audio.js
+++ audio.js
@@ -1,34 +1,31 @@
 var data = {
     title: [
-       
-        "Call Out my name",
-        "Do I wanna know",
-        "Freaked out",
+        "Call_Out_my_name",
+        "Do_I_wanna_know",
+        "Freaked_out",
         "Gladiator",
-        "Mala ft Anuel AA",
-        "São Paulo",
+        "Mala_ft_Anuel_AA",
+        "São_Paulo",
         "Secret",
         "Swim",
-        "The Walls",
+        "The_Walls",
         "Timeless"
 
     ],
-    song: [
-      
-        "music/Call Out my name.mp3",
-        "music/Do I wanna know.mp3",
-        "music/Freaked out.mp3",
+    song: [     
+        "music/Call_Out_my_name.mp3",
+        "music/Do_I_wanna_know.mp3",
+        "music/Freaked_out.mp3",
         "music/Gladiator.mp3",
-        "music/Mala ft Anuel AA.mp3",
-        "music/São Paulo.mp3",
+        "music/Mala_ft_Anuel_AA.mp3",
+        "music/São_Paulo.mp3",
         "music/Secret.mp3",
         "music/Swim.mp3",
-        "music/The Walls.mp3",
+        "music/The_Walls.mp3",
         "music/Timeless.mp3"
     ],
     
     poster: [
-      
         "https://i.pinimg.com/1200x/c6/b2/33/c6b2339d79aa7bf4ad318235d19b618f.jpg",
         "https://i.pinimg.com/1200x/fc/2e/22/fc2e22b867d7ccf7158aeb7b56ec9a40.jpg",
         "https://i.pinimg.com/1200x/71/65/b5/7165b53977fb6cbb554ec8c64459ccc1.jpg",
@@ -73,10 +70,77 @@ function playOrPauseSong(){
     }else {
         song.pause()
         play.src = "images/play-button-arrowhead.png"
+    
     }
 
 }
 
+song.addEventListener("timeupdate",function(){
+    let fill = document.getElementById("fill")
+    let position = song.currentTime / song.duration
+    fill.style.width = position * 100 + "%"
+    convertTime(song.currentTime)
+
+    if(song.ended){
+        next()
+
+    }
+    
+})
 
 
+function convertTime(seconds){
+    let currentTime = document.getElementById("currentTime")
+    let min = Math.floor(seconds/60)
+    let sec = Math.floor(seconds%60)
+
+    min = (min < 10) ? "0" + min : min
+    sec = (sec < 10) ? "0" + sec : sec
+    currentTime.textContent = min + ":" + sec 
+    totalTime(Math.round(song.duration))
+}
+
+function totalTime(seconds){
+    let min = Math.floor(seconds/60)
+    let sec = Math.floor(seconds%60)
+
+    min = (min < 10) ? "0" + min : min
+    sec = (sec < 10) ? "0" + sec : sec
+    currentTime.textContent += "/" + min + ":" + sec 
+   
+
+}
+
+function next(){
+    currentSong++
+    if(currentSong >= data.song.length){
+        currentSong = 0
+
+    }
+    playSong()
+    play.src = "images/pause.png"
+
+}
+
+function pre(){
+    currentSong--
+    if(currentSong <= 0){
+        currentSong = data.song.length-1
+    }
+    playSong()
+     play.src = "images/pause.png"
+
+
+}
+
+function muted (){
+    let mute = document.getElementById("mute")
+    if(song.muted){
+        song.muted = false 
+        mute.src = "images/volume.png"        
+    }else{
+        song.muted = true 
+        song.src = "images/volume-mute.png"
+    }
+}
 
